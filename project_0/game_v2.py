0cc8b233e189e2335b1ccf674ff1db6843567227
Gorelov <molderrus@mail.ru>
2023-05-15 21:29:11 +0300

    3

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -11,6 +11,7 @@ def random_predict(number:int=1) ->int:
     return(count)
 print(f'Количество попыток:{random_predict()}')
 def score_game(random_predict) -> int:
+    
     count_ls=[]# список для сохранения количества попыток
     np.random.seed(1)#фиксирует сид для воспроизводимости
     random_array = np.random.randint(1,101, size=(1000))#загадали список чисел
@@ -19,7 +20,6 @@ def score_game(random_predict) -> int:
         count_ls.append(random_predict(number))
         
     score=int(np.mean(count_ls))#находим среднее количество попыток
-    
     
     print(f'Ваш алгоритм угадывает число в среднем за:{score} попыток')
     return(score)
